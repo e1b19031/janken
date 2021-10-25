83103395a79c2f53da8d8131b74d009234bac055
e1b19031 <amazki@icloud.com>
2021-10-25 18:14:02 +0900

    ユーザー情報表示

---
+++ .history/src/main/java/oit/is/z1649/sato/janken/model/User_20211024235210.java
@@ -0,0 +1,20 @@
+package oit.is.z1649.sato.janken.model;
+
+public class User{
+    int id;
+    String name;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+}
